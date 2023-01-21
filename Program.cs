810549e6b83eef26fd99b3e348fd881923375335
fikri-kemal-adam <fb_kemal_1998_@hotmail.com>
2023-01-21 18:53:10 +0300

    first_iteration

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace try_catch;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayi giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmis oldugunuz sayi:"+ sayi);
+
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Hata:"+ex.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("Islem tamamlandi.");
+        }
+
+        try
+        {
+            //int a = int.Parse(null);
+            int a = int.Parse("test");
+        }
+        catch(ArgumentNullException ex)
+        {
+            Console.WriteLine("Bos deger girdiniz!!");
+            Console.WriteLine(ex);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Veri tipi uygun degil!!");
+            Console.WriteLine(ex);
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Description
+- This project prepared for making pratice on yazilimakademisi lessons.
